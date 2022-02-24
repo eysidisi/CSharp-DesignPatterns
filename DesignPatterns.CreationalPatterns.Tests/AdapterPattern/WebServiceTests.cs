5ebf02369457c81b8cd1c27f66563e735628935b
ali elmas <a.ihsan.elmas@gmail.com>
2022-02-24 11:48:13 +0300

    Start implementing Adapter Pattern

---
+++ DesignPatterns.CreationalPatterns.Tests/AdapterPattern/WebServiceTests.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace DesignPatterns.CreationalPatterns.Tests.AdapterPattern
+{
+    public class WebServiceTests
+    {
+        [Fact]
+        public void Request_ValidJson_Returns200()
+        {
+            // Arrange
+
+            // Act
+
+            // Assert
+        }
+    }
+}
---
+++ DesignPatterns.CreationalPatterns/AdapterPattern/WebService.cs
@@ -0,0 +1,24 @@
+﻿namespace DesignPatterns.CreationalPatterns.AdapterPattern
+{
+    public class WebService
+    {
+        public int Request(string json)
+        {
+            if (json.ToUpper() == "a valid object".ToUpper())
+            {
+                return 200;
+            }
+
+            else if (json.ToUpper() == "an invalid object".ToUpper())
+            {
+                return 400;
+            }
+
+            else
+            {
+                return 403;
+            }
+
+        }
+    }
+}
